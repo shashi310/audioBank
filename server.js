e5caab98b0b7f916949ed0b4293d0b828c8b7e24
Shashikant Yadav <103162888+shashi310@users.noreply.github.com>
2024-02-22 13:17:56 +0530

    tewst

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 8080;
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname); 
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// Route to handle file upload
+app.post('/upload', upload.single('audio'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded.' });
+  }
+  
+  const uploadedAudio = req.file;
+  const audioUrl = `${req.protocol}://${req.get('host')}/audio/${uploadedAudio.filename}`;
+  res.status(200).json({ url: audioUrl });
+});
+
+// Route to serve uploaded audio file
+app.get('/audio/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'uploads', filename);
+  console.log("audio is playing");
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (err) {
+      return res.status(404).json({ error: 'Audio file not found.' });
+    }
+
+    res.sendFile(filePath);
+  });
+});
+
+
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
